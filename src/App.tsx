94ac1c5d834080d3a1561e4a260dda4d6070f38b
Konrad <konrad.domalewski@gmail.com>
2023-12-05 14:07:57 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <Button color="primary" size="lg">
+      Button
+    </Button>
+  );
+}
