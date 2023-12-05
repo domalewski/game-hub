e79263c6c96240f2e45fc4e34fa6a55a49ff0354
Konrad <konrad.domalewski@gmail.com>
2023-12-05 15:55:13 +0100

    add responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import NavBar from "./components/NavBar";
 export default function App() {
   return (
     <>
-      <NavBar />
+      <div className="grid grid-rows-2 grid-col-2 gap-4">
+        <div className="col-span-2">
+          <NavBar />
+        </div>
+        <div className="hidden lg:flex">aside</div>
+        <div>main</div>
+      </div>
     </>
   );
 }
