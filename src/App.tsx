ea1eb0df80ee2a582dfdfdc9370de2392d82c761
Konrad <konrad.domalewski@gmail.com>
2023-12-07 13:38:50 +0100

    add genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenereList from "./components/GenreList";
 
 export default function App() {
   return (
     <>
       <NavBar />
       <div className="flex">
-        <div className="hidden lg:flex flex-none">aside</div>
+        <div className="hidden lg:flex flex-none">
+          <GenereList />
+        </div>
         <div className="flex-auto">
           <GameGrid />
         </div>
