2d2c6a98f2f25d8c1c46f5272f16f096e6e2f5ff
Konrad <konrad.domalewski@gmail.com>
2023-12-07 12:40:28 +0100

    optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,20 +2,20 @@ import { Card, CardBody, Image } from "@nextui-org/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticsScore from "./CriticsScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface GameCardProps {
   game: Game;
 }
 
 const GameCard = ({ game }: GameCardProps) => {
+  const imageUrl = getCroppedImageUrl(game.background_image);
+  console.log(imageUrl);
+
   return (
     <Card>
       <CardBody className="overflow-visible p-0">
-        <Image
-          alt="Card background"
-          className="object-cover rounded-sm"
-          src={game.background_image}
-        />
+        <Image className="object-cover rounded-sm" src={imageUrl} />
         <div className="flex flex-col p-1">
           <p className="text-2xl">{game.name}</p>
           <div className="flex flex-row justify-between">
